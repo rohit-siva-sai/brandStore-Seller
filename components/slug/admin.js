fdf7ad8be4627fceaac57474f0a307bc83652e81
rohit-siva-sai <106007387+rohit-siva-sai@users.noreply.github.com>
2023-09-14 17:39:41 +0530

    toast commit

--- components/slug/admin.js
+++ components/slug/admin.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 import Order from "./adminDetails/order";
 import UnitPrice from "./adminDetails/unitPrice";
 import ValidTo from "../rfq/productInfo/validTo";
@@ -10,16 +9,10 @@ import { Seller } from "@/useStore/seller";
 import { collection, doc, setDoc } from "firebase/firestore";
 import { db } from "@/config/firebase";
 import { Progress } from "antd";
+import { Toaster, toast } from "react-hot-toast";
 
-const Admin = ({id}) => {
-  const [
-    
-    order,
-    unitPrice,
-    requirements,
-   progress
-  ] = Seller((store) => [
-   
+const Admin = ({ id }) => {
+  const [order, unitPrice, requirements, progress] = Seller((store) => [
     store.order,
     store.unitPrice,
     store.requirements,
@@ -28,46 +21,56 @@ const Admin = ({id}) => {
   ]);
   const quotationsCollection = collection(db, "quotations");
 
-
   const submitNewQuotation = async () => {
     try {
       await setDoc(doc(quotationsCollection), {
-        
         order: order,
         unitPrice: unitPrice,
         requirements: requirements,
-       
+
         quotationScore: progress,
-        rfqId: id
-      })
-      
+        rfqId: id,
+      });
+      toast.success("Quotation Successfull", {
+        duration: 1000,
+        position: "top-center",
+        style: {
+          background: "black",
+          color: "white",
+        },
+      });
     } catch (err) {
-      console.log(err,"rohitk skvsman ");
+      console.log(err, "rohitk skvsman ");
     }
   };
-  console.log('rohit',order,unitPrice,requirements,progress);
-  const twoColors = { '0%': '#108ee9', '100%': '#87d068' };
-  
+  console.log("rohit", order, unitPrice, requirements, progress);
+  const twoColors = { "0%": "#108ee9", "100%": "#87d068" };
+
   return (
     <div className="w-3/5 px-8 bg-white rounded-xl h-[900px] py-4">
+      <Toaster />
+
       {/* <p className="font-bold text-xl px-6 text-gray-800 mb-4">
         Quotes Received (0 of Maximun 20)
       </p> */}
-      <Progress percent={progress} status="active" strokeColor={{ from: '#108ee9', to: '#87d068' }} />
+      <Progress
+        percent={progress}
+        status="active"
+        strokeColor={{ from: "#108ee9", to: "#87d068" }}
+      />
       <div className="flex flex-col space-y-4">
-        <Order/>
-        <UnitPrice/>
-        <ValidTo/>
+        <Order />
+        <UnitPrice />
+        <ValidTo />
         <Require />
         <div
-            onClick={() => {
-            submitNewQuotation()            
-            }}
-          
-            className="cursor-pointer mt-5 text-base w-fit font-semibold px-7 rounded-md text-white bg-gradient-to-l from-blue-400  to-blue-600 py-2"
-          >
-            Submit
-          </div>
+          onClick={() => {
+            submitNewQuotation();
+          }}
+          className="cursor-pointer mt-5 text-base w-fit font-semibold px-7 rounded-md text-white bg-gradient-to-l from-blue-400  to-blue-600 py-2"
+        >
+          Submit
+        </div>
       </div>
     </div>
   );
